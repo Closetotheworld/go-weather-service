c9420b0aa3059508347935366ca0c8e48cdb6a87
closetotheworld <wall.heo@lunit.io>
2022-11-09 23:50:35 +0900

    add: test async request for speed to response

--- pkg/weather/weather_test.go
+++ pkg/weather/weather_test.go
@@ -6,48 +6,48 @@ import (
 )
 
 var (
-	API_KEY = ""
+	API_KEY = "CMRJW4WT7V3QA5AOIGPBC"
 	w       = WeatherApiManagerImpl{}
 )
 
 func TestWeatherApiManagerImpl_GetCurrentInfo(t *testing.T) {
 	t.Run("test valid request", func(t *testing.T) {
 		w.ApiKey = API_KEY
-		result, err := w.GetCurrentInfo(10, -120)
+		result, err := w.GetCurrentInfo("10", "-120")
 		assert.NotNil(t, result)
 		assert.Nil(t, err, nil)
+		t.Log(result)
 	})
 	t.Run("test bad request", func(t *testing.T) {
 		w.ApiKey = API_KEY
-		result, err := w.GetCurrentInfo(-100, -200)
+		result, err := w.GetCurrentInfo("-100", "-200")
 		assert.Nil(t, result)
 		assert.NotNil(t, err)
 	})
 	t.Run("test unauthrized", func(t *testing.T) {
 		w.ApiKey = ""
-		result, err := w.GetCurrentInfo(-100, -200)
-		t.Log(result)
-		assert.NotEqual(t, err, nil)
+		_, err := w.GetCurrentInfo("-100", "-200")
+		assert.NotNil(t, err)
 	})
 }
 
 func TestWeatherApiManagerImpl_GetForecastInfo(t *testing.T) {
 	t.Run("test valid request", func(t *testing.T) {
 		w.ApiKey = API_KEY
-		result, err := w.GetForecastInfo(10, -120, 12)
+		result, err := w.GetForecastInfo("10", "-120", "12")
+		t.Log(result)
 		assert.NotNil(t, result)
 		assert.Nil(t, err, nil)
 	})
 	t.Run("test bad request", func(t *testing.T) {
 		w.ApiKey = API_KEY
-		result, err := w.GetForecastInfo(-100, -200, 5)
+		result, err := w.GetForecastInfo("-100", "-200", "5")
 		assert.Nil(t, result)
 		assert.NotNil(t, err)
 	})
 	t.Run("test unauthorized", func(t *testing.T) {
 		w.ApiKey = ""
-		result, err := w.GetForecastInfo(-100, -200, 16)
-		t.Log(result)
+		_, err := w.GetForecastInfo("-100", "-200", "16")
 		assert.NotEqual(t, err, nil)
 	})
 }
@@ -55,19 +55,33 @@ func TestWeatherApiManagerImpl_GetForecastInfo(t *testing.T) {
 func TestWeatherApiManagerImpl_GetHistoricalInfo(t *testing.T) {
 	t.Run("test valid request", func(t *testing.T) {
 		w.ApiKey = API_KEY
-		result, err := w.GetHistoricalInfo(10, -120, 6)
+		result, err := w.GetHistoricalInfo("10", "-120", "-6")
+		t.Log(result)
 		assert.NotNil(t, result)
 		assert.Nil(t, err, nil)
 	})
 	t.Run("test bad request", func(t *testing.T) {
 		w.ApiKey = API_KEY
-		result, err := w.GetHistoricalInfo(-100, -200, -10)
+		result, err := w.GetHistoricalInfo("-100", "-200", "-10")
 		assert.Nil(t, result)
 		assert.NotNil(t, err)
 	})
 	t.Run("test unauthorized", func(t *testing.T) {
-		result, err := w.GetHistoricalInfo(-100, -200, 2)
-		t.Log(result)
+		_, err := w.GetHistoricalInfo("-100", "-200", "2")
 		assert.NotEqual(t, err, nil)
+	})
+}
+
+func TestWeatherApiManagerImpl_AsyncRequest(t *testing.T) {
+	t.Run("run", func(t *testing.T) {
+		w.ApiKey = API_KEY
+		current, forecast, historical := w.AsyncRequest("10", "-120")
+		t.Log(current)
+		for i := range forecast {
+			t.Log(forecast[i])
+		}
+		for i := range historical {
+			t.Log(historical[i])
+		}
 	})
 }

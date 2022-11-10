46d5346f14092bdbe60375468c6be33f011695b4
closetotheworld <wall.heo@lunit.io>
2022-11-11 00:16:00 +0900

    add: summary service test with testcase

--- internal/service/weather_test.go
+++ internal/service/weather_test.go
@@ -6,28 +6,63 @@ import (
 
 	// external packages
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 
 	// project packages
 	"github.com/closetotheworld/go-weather-service/pkg/weather"
 )
 
 func TestNewWeatherService(t *testing.T) {
-	mockWeatherManager := weather.MockWeatherApiImpl{}
-	s := NewWeatherService(&mockWeatherManager)
-	assert.Equal(t, s, WeatherService{})
+	mockWeatherManager := weather.NewMockWeatherApiManager(t)
+	s := NewWeatherService(mockWeatherManager)
+	assert.NotNil(t, s)
 }
 
 func TestWeatherService_GetWeatherSummary(t *testing.T) {
 	ctx := context.Background()
-	mockWeatherManager := weather.MockWeatherApiImpl{}
-	ws := NewWeatherService(&mockWeatherManager)
 
-	w, err := ws.GetWeatherSummary(ctx)
-	assert.Nil(t, err)
-	t.Log(w)
 	// gretting test case
+	t.Run("greeting test", func(t *testing.T) {
+		for i := 0; i < 8; i++ {
+			mockWeatherManager := weather.NewMockWeatherApiManager(t)
+			ws := NewWeatherService(mockWeatherManager)
+
+			wc, result := GetGreetingTestCase(i)
+			mockWeatherManager.On("AsyncRequest", mock.Anything, mock.Anything).Return(wc.Current, wc.Forecast, wc.Historical, nil)
+
+			w, err := ws.GetWeatherSummary(ctx, "10", "120")
+			assert.Nil(t, err)
+			assert.Equal(t, result, w.Gretting)
+		}
+	})
 
 	// temperture test case
+	t.Run("temperture test", func(t *testing.T) {
+		for i := 0; i < 6; i++ {
+			mockWeatherManager := weather.NewMockWeatherApiManager(t)
+			ws := NewWeatherService(mockWeatherManager)
+
+			wc, result := GetTempertureTestCase(i)
+			mockWeatherManager.On("AsyncRequest", mock.Anything, mock.Anything).Return(wc.Current, wc.Forecast, wc.Historical, nil)
+
+			w, err := ws.GetWeatherSummary(ctx, "10", "120")
+			assert.Nil(t, err)
+			assert.Equal(t, result, w.Temperture)
+		}
+	})
 
 	// headsUp test case
+	t.Run("headsUp test", func(t *testing.T) {
+		for i := 0; i < 5; i++ {
+			mockWeatherManager := weather.NewMockWeatherApiManager(t)
+			ws := NewWeatherService(mockWeatherManager)
+
+			wc, result := GetHeadsUpTestCase(i)
+			mockWeatherManager.On("AsyncRequest", mock.Anything, mock.Anything).Return(wc.Current, wc.Forecast, wc.Historical, nil)
+
+			w, err := ws.GetWeatherSummary(ctx, "10", "120")
+			assert.Nil(t, err)
+			assert.Equal(t, result, w.HeadsUp)
+		}
+	})
 }

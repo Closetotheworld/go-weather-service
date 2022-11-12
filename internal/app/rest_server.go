76c903f3baba459c37e4b323e23b81e9d694a702
closetotheworld <37994634+Closetotheworld@users.noreply.github.com>
2022-11-12 19:34:53 +0900

    Add: swagger for api document (#10)
    * Add: add gin-swagger into server
    
    * Add: readme update for swagger

--- internal/app/rest_server.go
+++ internal/app/rest_server.go
@@ -3,10 +3,14 @@ package app
 import (
 	"log"
 	"strings"
+
 	// external packages
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 
 	// project packages
+	"github.com/closetotheworld/go-weather-service/docs"
 	"github.com/closetotheworld/go-weather-service/internal/app/middleware"
 	"github.com/closetotheworld/go-weather-service/internal/controller"
 	"github.com/closetotheworld/go-weather-service/internal/service/weather"
@@ -15,12 +19,16 @@ import (
 
 func StartServer(apiKey string, port string) {
 	r := gin.Default()
-
 	r.Use(middleware.TimeOutHandler())
+
+	docs.SwaggerInfo.Title = "Weather Summary api"
+	docs.SwaggerInfo.Description = "API documentation for Weather Summary api"
+	docs.SwaggerInfo.BasePath = ""
+	docs.SwaggerInfo.Schemes = []string{"http"}
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	weatherService := weather.NewWeatherService(&weather_api.WeatherApiManagerImpl{ApiKey: apiKey})
 	weatherController := controller.NewWeatherHeandler(weatherService)
-
 	r.GET("/summary", weatherController.GetWeatherSummary)
 
 	if err := r.Run(strings.Join([]string{":", port}, "")); err != nil {

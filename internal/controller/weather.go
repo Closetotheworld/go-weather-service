e9e4d92c2788747fd3a33bba16233b849aa939cb
closetotheworld <wall.heo@lunit.io>
2022-11-12 19:32:57 +0900

    Add: add gin-swagger into server

--- internal/controller/weather.go
+++ internal/controller/weather.go
@@ -24,18 +24,29 @@ func NewWeatherHeandler(weatherService domain.WeatherService) *WeatherHandler {
 	return &WeatherHandler{weatherService: weatherService}
 }
 
+// GetWeatherSummary
+// @Summary GetWeatherSummary
+// @Description weather에 대한 summary를 반환환다.
+// @Accept  json
+// @Produce  json
+// @Param default query InputWeatherSummary true "summary inputs"
+// @Router /summary [get]
+// @Success 200 {object} model.WeatherResult "Success"
+// @Failure 400 "Invalid parameter requested"
+// @Failure 408 "request timeout"
+// @Failure 500 "Internal Server error"
 func (w *WeatherHandler) GetWeatherSummary(c *gin.Context) {
 	inputQuery := InputWeatherSummary{}
 
 	if err := c.ShouldBindQuery(&inputQuery); err != nil {
-		c.Status(http.StatusBadRequest)
+		c.JSON(http.StatusBadRequest, "Invalid parameter requested")
 		log.Print(err.Error())
 		return
 	}
 
 	result, err := w.weatherService.GetWeatherSummary(c, inputQuery.Lat, inputQuery.Lon)
 	if err != nil {
-		c.Status(http.StatusInternalServerError)
+		c.JSON(http.StatusInternalServerError, "Internal Server error")
 		return
 	}
 	c.JSON(http.StatusOK, result)
